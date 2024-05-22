b7127aae402a040fa91747864056817096ff40b1
Kevin Mathew <96816115+kevinmathew47@users.noreply.github.com>
2024-05-22 20:23:53 +0530

    Create Program 2.vb

---
+++ Program 2.vb
@@ -0,0 +1,19 @@
+Public Class Form1
+
+ Private Sub Button1_Click(sender As Object, e As EventArgs) Handles 
+
+Button1.Click
+
+ MsgBox("You have selected button 1!!!!")
+
+ End Sub
+
+ Private Sub Button2_Click(sender As Object, e As EventArgs) Handles 
+
+Button2.Click
+
+ MsgBox("You have selected button 2!!!!")
+
+ End Sub
+
+End Class
